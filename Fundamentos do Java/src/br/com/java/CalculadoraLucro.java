81f50b05027a4be079ce53e69c072a32abec6769
gabriel.dbmiranda <gabriel.dbmiranda@TAT0541122W10-1.senacsp.edu.br>
2018-06-04 16:38:07 -0300

    Exercícios Lógicas de Programação, professor José de Assis.

---
+++ Fundamentos do Java/src/br/com/java/CalculadoraLucro.java
@@ -0,0 +1,27 @@
+package br.com.java;
+
+import java.util.Scanner;
+
+public class CalculadoraLucro {
+
+	public static void main(String[] args) {
+		double custo, lucro, venda;
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Calculadora do lucro");
+        System.out.println("");
+        // Entrada
+        System.out.print("Preço de custo: ");
+        custo = teclado.nextDouble();
+        System.out.print("% de lucro: ");
+        lucro = teclado.nextDouble();
+        // Processamento
+        venda = custo + ((custo * lucro) / 100);
+        // Saida
+        System.out.print("Preço de venda: " + venda);
+        
+        
+               
+        
+	}
+
+}

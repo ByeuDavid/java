967025e387aa55f2d7246cc9d16fed4443ececc0
gabriel.dbmiranda <gabriel.dbmiranda@TAT0541122W10-1.senacsp.edu.br>
2018-06-20 16:28:07 -0300

    Avaliação Churrascoladora

---
+++ Fundamentos do Java/src/br/com/java/Churrascoladora.java
@@ -0,0 +1,57 @@
+/**
+ * Atividade de avaliação - Churrascoladora
+ * @author Gabriel David, Sabrina da Silva
+ */
+
+package br.com.java;
+
+import java.util.Scanner;
+
+public class Churrascoladora {
+
+	public static void main(String[] args) {
+		// Variáveis
+		double homens, mulheres, criancas, carne, cerveja, refri, total, valorPessoa,
+				totalCarne, totalCerveja, totalRefri;
+		Scanner teclado = new Scanner(System.in);
+		// Entrada
+		System.out.println("====== CHURRASCOLADORA ======");
+		System.out.println("");
+		System.out.print("Quantidade de homens: ");
+		homens = teclado.nextDouble();
+		System.out.print("Quantidade de mulheres: ");
+		mulheres = teclado.nextDouble();
+		System.out.print("Quantidade de crianças: ");
+		criancas = teclado.nextDouble();
+		System.out.print("Preço médio do Kg de carne: ");
+		carne = teclado.nextDouble();
+		System.out.print("Preço médio da lata de cerveja: ");
+		cerveja = teclado.nextDouble();
+		System.out.print("Preço médio da lata de refri: ");
+		refri = teclado.nextDouble();
+		// Processamento
+		totalCerveja = (homens * 12) + (mulheres * 4); 
+		totalRefri = (criancas * 2 ) + (mulheres * 2);
+		totalCarne = ((homens + mulheres) * 0.3 + (criancas * 0.1));
+		total = (totalCerveja * cerveja + totalCarne * carne + totalRefri * refri);
+		valorPessoa = (total / (homens + mulheres));
+		
+
+		// Saída
+		System.out.println("");
+		System.out.println("_____________________________________________________");
+		System.out.println("");
+		System.out.println("Lista de compras");
+		// Exibir quantidade e preço
+		System.out.println("Quantidade total de carne: " + totalCarne + "kg " + " | " + "R$: "+ totalCarne * carne);
+		System.out.println("Quantidade total de cerveja: " + totalCerveja + " latas" + " | "+ "R$: " + totalCerveja * cerveja);
+		System.out.println("Quantidade total de refri: " + totalRefri + " latas" + " | "+ "R$: " + totalRefri * refri);
+		System.out.println("");
+		System.out.println("_____________________________________________________");
+		System.out.println("");
+		System.out.println("Valor total do churras: " + "R$: " + total );
+		System.out.println("Valor por pessoa: " + "R$: " + valorPessoa);
+
+	}
+
+}
